33ed809b0dcaf435e6c4447b406da17a431e075c
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2023-12-19 20:26:52 +0100

    update day 1 but seem to not work as expeted even though it gives the right anser for the test data

---
+++ day1/challange2.js
@@ -0,0 +1,83 @@
+
+// works with test data but not with the input data or some reason
+
+const fs = require('fs');
+
+let SUM = 0;
+
+const spelledNums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+try {
+    const fileContent = fs.readFileSync('input.txt', 'utf-8');
+    const data = fileContent.split('\n');
+    data.forEach(line => {
+        const result = trebuchet(line);
+        console.log(result)
+    });
+} catch (error) {
+    console.error('Error reading the file:', error.message);
+}
+
+function trebuchet(str) {
+    let first = findFirstNumber(str);
+    let last = findLastNumber(str);
+
+    if (first.length > 1) first = String(spelledNums.indexOf(first) + 1);
+    if (last.length > 1) last = String(spelledNums.indexOf(last) + 1);
+
+    const concatNums = first + last;
+    console.log(concatNums)
+    SUM += Number(concatNums);
+    return SUM
+}
+
+function findFirstNumber(str) {
+    const firstAppearingNum = {
+        number: '',
+        index: Infinity,
+    };
+
+    for (let spelledNum of spelledNums) {
+        const foundSpelledNumIndex = str.search(spelledNum);
+        if (foundSpelledNumIndex !== -1 && foundSpelledNumIndex < firstAppearingNum['index']) {
+            firstAppearingNum['number'] = spelledNum;
+            firstAppearingNum['index'] = foundSpelledNumIndex;
+        }
+    }
+
+    const numRegex = /\d/g;
+    const firstNumIndex = str.search(numRegex);
+    if (firstNumIndex !== -1 && firstNumIndex < firstAppearingNum['index']) {
+        firstAppearingNum['number'] = str.slice(firstNumIndex, firstNumIndex + 1);
+        firstAppearingNum['index'] = firstNumIndex;
+    }
+
+    return firstAppearingNum['number'];
+}
+
+function findLastNumber(str) {
+    const lastAppearingNum = {
+        number: '',
+        index: -1,
+    };
+
+    for (let spelledNum of spelledNums) {
+        const foundSpelledNumIndex = str.search(spelledNum);
+        if (foundSpelledNumIndex !== -1 && foundSpelledNumIndex > lastAppearingNum['index']) {
+            lastAppearingNum['number'] = spelledNum;
+            lastAppearingNum['index'] = foundSpelledNumIndex;
+        }
+    }
+
+    const numRegex = /\d/g;
+    const matches = str.match(numRegex);
+    const lastNumIndex = matches ? str.lastIndexOf(matches[matches.length - 1]) : -1;
+        if (lastAppearingNum['index'] < lastNumIndex) {
+            lastAppearingNum['number'] = str[lastNumIndex];
+            lastAppearingNum['index'] = lastNumIndex;
+        }
+
+    return lastAppearingNum['number'];
+}
+
+
