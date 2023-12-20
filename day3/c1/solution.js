219d428b184c630a544ac3f3bd466fc478d34737
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2023-12-20 13:51:08 +0100

    finish day3 challange 1

---
+++ day3/c1/solution.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+
+let SUM = 0;
+const schematic = [];
+
+try {
+    const fileContent = fs.readFileSync('input.txt', 'utf-8');
+    const data = fileContent.split('\n');
+    data.forEach(line => {
+        schematic.push(line.trim().split(''));
+    });
+} catch (err) {
+    console.log(err);
+}
+
+const rows = schematic.length;
+const cols = schematic[0].length
+findEngineParts(schematic, rows, cols)
+
+console.log(SUM) // 528819
+
+function findEngineParts(schematic, rows, cols) {
+    let start = null;
+    let end =  null;
+    for (let row = 0; row < rows; row++) {
+        for(let col = 0; col < cols; col++) {
+            if(!isNaN(parseInt(schematic[row][col]))){
+                start = col
+                while(!isNaN(parseInt(schematic[row][col]))) {
+                    col++
+                    end = col
+                }
+                const part = schematic[row].join('').slice(start, end)
+                if(isValidpart(schematic, row, start, end)){
+                    // console.log(part)
+                    SUM += parseInt(part)
+                }
+            }
+        }
+    }
+}
+
+function isValidpart(schematic, row, start, end){
+    const symbols= ['+','-','%','=','!','?','*','&','$','#','/','@' ]
+    if (schematic[row][start - 1] !== '.' && schematic[row][start - 1] !== undefined) {
+        // console.log(schematic[row][start - 1])
+        return true
+    }
+    
+    if(schematic[row][end] !== '.' && schematic[row][end] !== undefined) {
+        // console.log(schematic[row][end])
+        return true
+    }
+
+    for(let i = start-1; i < end+1; i++) {
+        if(schematic[row-1] && symbols.includes(schematic[row-1][i])){
+            // console.log(schematic[row-1][i])
+            return true
+        }
+        if(schematic[row+1] && symbols.includes(schematic[row+1][i])){
+            // console.log(schematic[row+1][i])
+            return true
+        }
+    }
+    return false
+}
+
