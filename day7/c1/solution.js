9b1c2dc90fdc2853e4d5b1d265bb3a77e6e7709c
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2024-01-10 21:34:16 +0100

    finsied day7 c1

--- day7/c1/solution.js
+++ day7/c1/solution.js
@@ -1,95 +1,139 @@
 const fs = require('fs');
 
-const players = [];
-let totalWinning = 0;
+let totalWinnings = 0
 
-try {
-    const fileContent = fs.readFileSync('input.txt', 'utf-8');
-    const data = fileContent.split('\n');
-    data.forEach((line, index) => {
-        const [hand, bid] = line.split(' ');
-        const type = getType(hand.split(''));
-        sortArrayOfPlayers(index, hand.split(''), Number(bid), type);
-    });
-} catch (err) {
-    console.log(err);
-}
-console.log(players)
-countWinnings()
+const five = []; 
+const four = [];
+const fullHouse = [];
+const three = [];
+const two = [];
+const one = [];
+const highCards = [];
 
-function countWinnings() {
-    let total = 0;
-    let rank = 1;
-  
-    for (let i = players.length - 1; i >= 0; i--) {
-      total += players[i].bid * rank;
-      rank++;
+getInputAndSortByType()
+
+function getInputAndSortByType(){
+    try {
+        const fileContent = fs.readFileSync('input.txt', 'utf-8');
+        const data = fileContent.split('\n');
+        data.forEach(line => {
+            const [hand, bid] = line.split(' ');
+            const bidInt = parseInt(bid, 10)
+
+            const type = getType(hand.split(''));
+
+            if(type === 5) five.push([hand, bidInt]);
+            if(type === 4) four.push([hand, bidInt]);
+            if(type === 3.2) fullHouse.push([hand, bidInt]);
+            if(type === 3) three.push([hand, bidInt]);
+            if(type === 2) two.push([hand, bidInt]);
+            if(type === 1) one.push([hand, bidInt]);
+            if(type === 0) highCards.push([hand, bidInt]);
+        });
+    } catch (err) {
+        console.log(err);
     }
-  
-    console.log(total);
-  }
+}
 
 function getType(hand) {
     const countCards = {}
     hand.forEach(card => countCards[card] =(countCards[card] || 0) + 1);
     const numOfDifferentCards = Object.keys(countCards).length
 
-    if(numOfDifferentCards === 1) return 7 //Five of a kind
+    if(numOfDifferentCards === 1) return 5 //Five of a kind
     if(numOfDifferentCards === 2) {
         if(Object.values(countCards).includes(4)) {
-            return 6
+            return 4
             // Four of a kind
         } else{
-            return 5
+            return 3.2
             // full house
         }
     }
     if(numOfDifferentCards === 3) {
         if(Object.values(countCards).includes(3)) { 
-            return 4
+            return 3
             //Three of a kind
         } else{
-            return 3
+            return 2
             // 'Two pair'
         }
     }
-    if(numOfDifferentCards === 4) return 2 //One pair
-    if(numOfDifferentCards === 5) return 1 //High card
+    if(numOfDifferentCards === 4) return 1 //One pair
+    if(numOfDifferentCards === 5) return 0 //High card
+}
+const sortFive = weakestToStrongestCards(five);
+const sortFour = weakestToStrongestCards(four);
+const sortFullHouse = weakestToStrongestCards(fullHouse);
+const sortThree = weakestToStrongestCards(three);
+const sortTwo = weakestToStrongestCards(two);
+const sortOne = weakestToStrongestCards(one);
+const sortHighCards = weakestToStrongestCards(highCards);
+
+const sortedHands = [ ...sortHighCards, ...sortOne, ...sortTwo, ...sortThree,...sortFullHouse,...sortFour, ...sortFive ]
+
+let rank = 0
+for (const hand of sortedHands) {
+    rank++
+    const bid = hand[1]
+    totalWinnings += bid * rank
 }
 
+console.log(totalWinnings)
 
 
-function sortArrayOfPlayers(player, hand, bid, type) {
-    if (player === 0) {
-      players.push({ hand: hand, type: type, bid: bid });
-      return
+function weakestToStrongestCards(arr){
+    if (arr.length <= 1) {
+        return arr;
+      }
+    
+      const middle = Math.floor(arr.length / 2);
+      const left = arr.slice(0, middle);
+      const right = arr.slice(middle);
+    
+      return merge(weakestToStrongestCards(left), weakestToStrongestCards(right));
     }
-  
-    for (let i = 0; i < players.length; i++) {
-        const currPlayer = players[i]
-        if(currPlayer.type === type) {
-            if(handRankHigherOrEqual(currPlayer.hand, hand)) {
-                players.splice(i, 0, { hand: hand, type: type, bid: bid });            
-                break;
-            } else {
-                continue;
-            }
+    
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    
+    while (leftIndex < left.length && rightIndex < right.length) {
+        const currLeft = left[leftIndex][0].split('')
+        const currRight = right[rightIndex][0].split('')
+        const winner = strongestCard(currLeft, currRight);
+
+        if(winner === 'L') {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else if(winner === 'R') {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        } else {
+            result.push(left[leftIndex]);
+            result.push(right[rightIndex]);
+            leftIndex++;
+            rightIndex++;
         }
-        if (players[i].type < type) {
-            players.splice(i, 0, { hand: hand, type: type, bid: bid});
-            break;
+    }
+return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+function strongestCard(left, right) {
+    const labels = [ '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' ]
+
+    for(let i = 0; i < left.length; i++) {
+
+        const cardLeftHand = labels.indexOf(left[i]);
+        const cardRightHand = labels.indexOf(right[i]);
+
+        if(cardLeftHand < cardRightHand){
+            return 'L'
         }
+        if(cardRightHand < cardLeftHand){
+            return 'R'
+        } 
     }
-  }
-
-function handRankHigherOrEqual(comapareHand, hand) {
-    const labels = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
-    for(let i = 0; i < hand.length; i++) {
-     const card1 = labels.indexOf(hand[i])
-     const card2 = labels.indexOf(comapareHand[i])
-     if(card1 < card2) return true // hand rank higher
-     if(card2 < card1) return false // comaperhand rank higher
-    }
-
-    return true //rank equal
+    return 0
 }
